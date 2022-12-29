edbaabbf4d604acd7f67217d91fa8b73e00ba0f2
pedro <pedrohrds05@hotmail.com>
2022-12-29 17:10:16 -0300

    feat: uri 1069 challenge

---
+++ java/URI1069.java
@@ -0,0 +1,54 @@
+package java;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+    https://www.beecrowd.com.br/judge/pt/problems/view/1069
+
+    João está trabalhando em uma mina, tentando retirar o máximo que consegue de diamantes "<>". Ele deve excluir todas as particulas de areia "." 
+    do processo e a cada retirada de diamante, novos diamantes poderão se formar. Se ele tem como uma entrada .<...<<..>>....>....>>>., três diamantes são formados. 
+    O primeiro é retirado de <..>, resultando  .<...<>....>....>>>. Em seguida o segundo diamante é retirado, restando .<.......>....>>>. O terceiro diamante é então retirado, restando no final .....>>>., 
+    sem possibilidade de extração de novo diamante.
+
+    Entrada
+    Deve ser lido um valor inteiro N que representa a quantidade de casos de teste. Cada linha a seguir é um caso de teste que contém até 1000 caracteres, incluindo "<,>, ."
+
+    Saída
+    Você deve imprimir a quantidade de diamantes possíveis de serem extraídos em cada caso de entrada.
+
+
+*/
+
+public class URI1069 {
+    public static void main(String[] args) throws IOException {
+ 
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		sc.nextLine();
+		
+		List<Integer> result = new ArrayList<>();
+		
+		for (int i = 0; i < n; i += 1) {
+			String input = sc.nextLine();
+			
+			int count = 0;
+
+			String removeDots = input.replaceAll("[.]", "");
+
+			while (removeDots.contains("<>")) {
+				removeDots = removeDots.replaceFirst("<>", "");
+				count += 1;
+			}
+
+			result.add(count);
+		}
+		
+		result.forEach(System.out::println);
+
+		sc.close();
+    }
+}
--- java/URI1168.java
+++ java/URI1168.java
@@ -8,6 +8,20 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
+/*
+    https://www.beecrowd.com.br/judge/pt/problems/view/1168
+
+    João quer montar um painel de leds contendo diversos números. Ele não possui muitos leds, e não tem certeza se conseguirá montar o número desejado. Considerando a configuração dos leds dos números abaixo, faça um algoritmo que ajude João a descobrir a quantidade de leds necessário para montar o valor.
+
+    Obs.: Para programadores de Javascript, recomenda-se o uso de "input.trim().split('\n')" para evitar erros conhecidos.
+
+    Entrada
+    A entrada contém um inteiro N, (1 ≤ N ≤ 1000) correspondente ao número de casos de teste, seguido de N linhas, cada linha contendo um número (1 ≤ V ≤ 10100) correspondente ao valor que João quer montar com os leds.
+
+    Saída
+    Para cada caso de teste, imprima uma linha contendo o número de leds que João precisa para montar o valor desejado, seguido da palavra "leds".
+*/
+
 public class URI1168 {
     public static Integer countLeds(String num) {
 		Map<String, Integer> ledsValue = new TreeMap<>();
