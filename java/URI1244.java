96b05b1acccf487486b9edd6d34adc5fc94d5a14
pedro <pedrohrds05@hotmail.com>
2022-12-29 21:29:33 -0300

    feat: uri 1244 challenge

---
+++ java/URI1244.java
@@ -0,0 +1,46 @@
+package java;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class URI1244 {
+    static void sort(String[] s, int n) {
+		for (int i = 1; i < n; i++) {
+			String temp = s[i];
+
+			int j = i - 1;
+
+			while (j >= 0 && temp.length() > s[j].length()) {
+				s[j + 1] = s[j];
+				j--;
+			}
+			s[j + 1] = temp;
+		}
+	}
+
+	static String serializeStringArr(String[] arr) {
+		return String.join(",", Arrays.asList(arr)).replace(",", " ");
+	}
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		sc.nextLine();
+
+		for (int i = 0; i < n; i += 1) {
+			String input = sc.nextLine();
+
+			String[] splitedStr = input.split(" ");
+
+			sort(splitedStr, splitedStr.length);
+
+			String result = serializeStringArr(splitedStr);
+
+			System.out.println(result);
+		}
+
+		sc.close();
+	}
+}
