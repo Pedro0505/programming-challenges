0c9899cf4cd8e924232e4ab8ef20e262adf6f64b
pedro <pedrohrds05@hotmail.com>
2022-12-30 10:23:57 -0300

    feat: uri 2006 challenge

---
+++ java/URI2006.java
@@ -0,0 +1,44 @@
+package java;
+
+import java.io.IOException;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+    Degustação de chá às escuras é a habilidade de identificar um chá usando apenas seus sentidos do olfato e paladar.
+
+    Isto faz parte da Competição Ideal de Consumidores de Chá Puro (da sigla em inglês ICPC), que um programa de TV local está organizando. 
+    Durante o show, um bule de chá completo é preparado e são entregues uma xícara de chá para cada um dos cinco competidores. Os participantes devem cheirar, 
+    saborear e avaliar a amostra, de modo a identificar o tipo de chá, que pode ser: (1) o chá branco; (2) chá verde; (3) chá preto; ou (4) chá de ervas. 
+    No final, as respostas são verificadas para determinar o número de suposições corretas.
+
+    Dado o tipo de chá real e as respostas fornecidas, determinar o número de participantes que receberam a resposta correta.
+
+    Entrada
+    A primeira linha contém um inteiro T representando o tipo de chá (1 ≤ T ≤ 4). A segunda linha contém cinco inteiros A, B, C, D e E, que indica a resposta dada por cada competidor (1 ≤ A, B, C, D, E ≤ 4).
+
+    Saída
+    A saída contém um inteiro representando o número de concorrentes que obtiveram a resposta correta.
+*/
+
+public class URI2006 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine();
+        
+        String choiceTeas = sc.nextLine();
+
+        List<String> teas = Arrays.asList(choiceTeas.split(""));
+
+        int winners = Collections.frequency(teas, String.valueOf(n));
+        
+        System.out.println(winners);
+
+        sc.close();
+    }
+}
