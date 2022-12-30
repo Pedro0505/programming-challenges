8c8702965d242c91422ef815db313e45d50590d9
Pedro Henrique <87395119+Pedro0505@users.noreply.github.com>
2022-12-29 21:34:03 -0300

    refactor(1244): add url to chanllenge

--- java/URI1244.java
+++ java/URI1244.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
+// https://www.beecrowd.com.br/judge/pt/problems/view/1244
+
 public class URI1244 {
-    static void sort(String[] s, int n) {
+    	static void sort(String[] s, int n) {
 		for (int i = 1; i < n; i++) {
 			String temp = s[i];
 
