8bf1d5594ff97b278a9a8f91ab8e2b332e1aa5e3
pedro <pedrohrds05@hotmail.com>
2023-01-03 15:32:00 -0300

    feat: uri 2867 challenge

---
+++ java/URI2867.java
@@ -0,0 +1,31 @@
+package java;
+
+import java.io.IOException;
+
+import java.util.*;
+
+public class URI2867 {
+    
+    public static long calcPow(int a, int b) {
+        return (long) Math.log10(Math.pow(a, b)) + 1;
+    }
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        sc.nextLine();
+        
+        for (int i = 0; i < n; i += 1) {
+        	String num = sc.nextLine();
+
+        	String[] splitedNum = num.split(" ");
+
+        	long response = calcPow(Integer.parseInt(splitedNum[0]), Integer.parseInt(splitedNum[1]));
+        	
+        	System.out.println(response);
+        }
+        
+        sc.close();
+    }
+}
