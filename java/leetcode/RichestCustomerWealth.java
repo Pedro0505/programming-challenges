145979dc5e23ba398bcee3625f5659564bd16396
pedro <pedrohrds05@hotmail.com>
2023-01-05 20:20:15 -0300

    feat(leetcode): adding Richest Customer Wealth challenge

---
+++ java/leetcode/RichestCustomerWealth.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class RichestCustomerWealth {
+    public static int maximumWealth(int[][] accounts) {
+		ArrayList<Integer> result = new ArrayList<>();
+
+		for (int i = 0; i < accounts.length; i += 1) {
+			int sum = 0;
+
+			for (int j = 0; j < accounts[i].length; j++) {
+				sum += accounts[i][j];
+			}
+
+			result.add(sum);
+		}
+		
+		Collections.sort(result);
+
+		return result.get(result.size() - 1);
+    }
+
+    public static void main(String[] args) {
+		int[][] arr = { { 1, 5 }, { 7, 3 }, { 3, 5 } };
+        System.out.println(maximumWealth(arr));
+    }
+}
