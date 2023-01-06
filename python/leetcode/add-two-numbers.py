7ed3e0e2fe8b42f7767fa3d9c69d3dfce6b02f74
pedro <pedrohrds05@hotmail.com>
2023-01-06 19:36:56 -0300

    refactor(leetcode): making more clean code

--- python/leetcode/add-two-numbers.py
+++ python/leetcode/add-two-numbers.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/add-two-numbers/submissions/872914121/
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -5,24 +7,21 @@ class ListNode:
 
 
 def addTwoNumbers(l1, l2):
-    first_link = l1
-    second_link = l2
-
     first_link_value = ''
     second_link_value = ''
 
-    while(first_link is not None):
-        if first_link is not None:
-            first_link_value += str(first_link.val)
+    while(l1 is not None):
+        if l1 is not None:
+            first_link_value = str(l1.val) + first_link_value
 
-            first_link = first_link.next
+            l1 = l1.next
 
-    while(second_link is not None):
-        second_link_value += str(second_link.val)
+    while(l2 is not None):
+        second_link_value = str(l2.val) + second_link_value
 
-        second_link = second_link.next
+        l2 = l2.next
 
-    sum = int(first_link_value[::-1]) + int(second_link_value[::-1])
+    sum = int(first_link_value) + int(second_link_value)
 
     new_linked_values = list(map(int, str(sum)))
 
@@ -34,15 +33,8 @@ def addTwoNumbers(l1, l2):
     new_linked_list = ListNode(new_linked_values[0])
     new_linked_values.pop(0)
     node = new_linked_list
-    for i, val in enumerate(new_linked_values):
+    for val in new_linked_values:
         node.next = ListNode(val)
         node = node.next
 
-    return node
-
-
-l1 = ListNode(2, ListNode(4, ListNode(3, None)))
-
-l2 = ListNode(5, ListNode(6, ListNode(4, None)))
-
-addTwoNumbers(l1, l2)
+    return new_linked_list
