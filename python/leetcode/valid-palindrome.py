d8c3fc021109377f1b232dd1f67250ce5eb434f6
pedro <pedrohrds05@hotmail.com>
2023-01-12 17:52:50 -0300

    feat(leetcode): adding is palindrome challenge

---
+++ python/leetcode/valid-palindrome.py
@@ -0,0 +1,9 @@
+import re
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        result = re.sub(r'[^a-zA-Z0-9]', '', s)
+        lower = result.lower()
+
+        return lower == lower[::-1]
