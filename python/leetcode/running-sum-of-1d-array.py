2f514f3dd319188fe4ac3a07b3c7b98363ec9d2c
pedro <pedrohrds05@hotmail.com>
2023-01-16 21:10:12 -0300

    feat(leetcode/python): adding running sum of 1d array challenge

---
+++ python/leetcode/running-sum-of-1d-array.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        ans = list()
+
+        for i in range(len(nums)):
+            ans.append(sum(nums[:i + 1]))
+
+        return ans
